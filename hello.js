856fcd37660cd4f054ceae1ef2a79e34e2ccbf5a
Anastasia <ketchersid18@mail.ru>
2026-08-01 16:29:39 -0500

    start hello-bun with hello script and setup notes

---
+++ hello.js
@@ -0,0 +1,4 @@
+let course = "Level 2";
+let message = "I am learning JS !";
+console.log(message);
+console.log("This JavaScript runs on MY machine.");
---
+++ notes.md
@@ -0,0 +1,5 @@
+# My Setup
+- OS: Windows
+- Shell: Git Bash
+- Bun version: 1,3,14
+- Editor: Notepad++
