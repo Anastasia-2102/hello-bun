7bf9d816f30ef5cf5d6f4356d6cf0446e4706385
Anastasia <ketchersid18@mail.ru>
2026-08-02 15:28:28 -0500

    update hello message

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 let course = "Level 2";
-let message = "I am learning JS !";
+let message = "I am learning JS!";
 console.log(message);
-console.log("This JavaScript runs on MY machine.");
+console.log("This JavaScript runs on MY computer.");
 console.log(new Date ());
